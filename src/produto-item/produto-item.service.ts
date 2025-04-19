be49c3412ac10dbb2932112b11e494ca91300e48
lmFreire <l.matheus.freire10@gmail.com>
2025-04-19 15:44:29 -0300

    fix: not null

--- src/produto-item/produto-item.service.ts
+++ src/produto-item/produto-item.service.ts
@@ -50,7 +50,10 @@ export class ProdutoItemService {
     async findAllByEmpresa(data: BuscarProdutoItemDto){
         return await this.prismaService.produtoItem.findMany({
             where: {
-                empresaId: Number(data.empresaId)
+                empresaId: Number(data.empresaId),
+                vendaItem: {
+                    NOT: null
+                }
             },
             include: {
                 produto: true
